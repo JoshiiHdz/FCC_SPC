4f01ab3f598a6d0b9514779669fba15a280e95ce
JoshiiHdz <135800215+JoshiiHdz@users.noreply.github.com>
2023-06-07 19:37:36 +0000

    new

--- db-connection.js
+++ db-connection.js
@@ -1,35 +1,18 @@
-const {MongoClient} = require('mongodb');
 
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://hdzjoshii:root@cluster0.gyqd0ka.mongodb.net/ffc?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+const db = mongoose.connect(process.env.DB,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+module.exports = db;
+
+
  
 
-    const client = new MongoClient(process.env.DB);
-    
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-       // await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-    module.exports = client;
-}
 
 
-
-main().catch(console.error);
 
 
 
